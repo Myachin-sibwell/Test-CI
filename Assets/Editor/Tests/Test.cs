3d997d39857ced56cae326743088feda8300fd23
myachin.e <myachin.sibwell@gmail.com>
2021-09-09 18:49:47 +0700

    Добавлен неправильный тест, убран аплоад лога теста

---
+++ Assets/Editor/Tests/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [Test]
+    public void Test1()
+    {
+        int test1 = 10;
+        int test2 = 10;
+        
+        Assert.IsTrue(test1 == test2);
+        
+        var a = test1 - 1;
+        var b = test2;
+        
+        Assert.IsTrue(a == b);
+    }
+}
